bbb8b4f833b55d51817c6afa1cb5f5c9247cb162
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2020-12-11 06:41:50 +0000

    plus git

--- git-convert-remote.sh
+++ git-convert-remote.sh
@@ -18,18 +18,20 @@ usage() {
   echo "  url = https://github.com/olegse/scripts ->  url = git@github.com-olegse:olegse/scripts.git"
   echo "    -t    display the resulting line (do not edit)"
   echo "    -i    edit a file"
+  echo "    -p    print current url"
   exit 1
 }
 
 #pwd
 if (( $# == 0 )); then usage; fi
 
-while getopts ":ti" OPT
+while getopts ":tip" OPT
 do
-  echo "Processing: $OPT"
+  #echo "Processing: $OPT"
   case $OPT in
    't') action='np' ;;
    'i') action='i' ;;
+   'p') action='c' ;;
    '?') echo "Invalid option '-$OPTARG'"
         usage ;;
   esac
@@ -39,6 +41,15 @@ done
 path=${!OPTIND:-.};
 GIT_CONF=$path/.git/config
 
+case $action in 
+  'c') awk -F= '/url/ {print $2}' $GIT_CONF
+       exit ;;
+esac
 echo "action: $action"
+
 set -x
-sed -${action:0:1} "/url/ s,[-a-zA-Z:._= ]\+//\([^/]\+\)/\([^/]\+\)/\([^/]\+\),git@\1-\2:\2/\3,${action:1:1}" $GIT_CONF
+if grep -q $GIT_CONF -e "url.*https"; then 
+  sed -${action:0:1} "/url/ s,\S\+//\([^/]\+\)/\([^/]\+\)/\([^/]\+\),git@\1:\2/\3,${action:1:1}" $GIT_CONF
+else
+  sed -${action:0:1} "/url/ s,git@\([^:]\+\):,https://\1/,${action:1:1}" $GIT_CONF
+fi
